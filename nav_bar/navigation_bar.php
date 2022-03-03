2c753218f65fa25edd8b430b1379f4b866da6c4a
PLEUTOSEEK <xuanvanquish@gmil.com>
2022-03-03 17:20:53 +0800

    navigation bar

--- nav_bar/navigation_bar.php
+++ nav_bar/navigation_bar.php
@@ -18,25 +18,32 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">LGTL Cineplex</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark align-items-center">
+            <a class="navbar-brand  " href="#">LGTL Cineplex</a>
+            <button class="navbar-toggler col-sm-lg-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                <ul class="nav nav-pills  navbar-nav " id="pills-tab" role="tablist">
-                    <li class="nav-item add-on-nav">
-                        <a class="nav-link active add-on-nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+            <div class="collapse navbar-collapse   col-lg justify-content-center" id="navbarSupportedContent">
+                <ul class="nav nav-pills  navbar-nav col-lg" id="pills-tab" role="tablist">
+                    <li class="nav-item mr-lg-4 col-lg ">
+                        <a class="nav-link active pl-sm-3 pl-3 mt-lg-1 mt-sm-3 mt-3 text-lg-center" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
                     </li>
-                    <li class="nav-item add-on-nav">
-                        <a class="nav-link add-on-nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
+                    <li class="nav-item mr-lg-4 col-lg ">
+                        <a class="nav-link  pl-sm-3 pl-3 mt-lg-1 mt-sm-3 mt-3 text-lg-center" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
                     </li>
-                    <li class="nav-item add-on-nav">
-                        <a class="nav-link add-on-nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+                    <li class="nav-item mr-lg-4 col-lg ">
+                        <a class="nav-link   pl-sm-3 pl-3 mt-lg-1 mt-sm-3 mt-3 text-lg-center" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
                     </li>
                 </ul>
+                <button class=" btn btn-light  col-lg-1 mt-lg-0 mt-sm-3 mt-3 " >
+                    LOG IN
+                </button>
             </div>
+
+
         </nav>
+
+
 
         <?php
         // put your code here
