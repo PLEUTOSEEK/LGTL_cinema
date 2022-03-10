9f5f0eb7050c0b92bfa22bb5fe4b2ac2cfda5019
limxy-wm19 <limxy-wm19@student.tarc.edu.my>
2022-03-10 16:55:50 +0800

    login

--- log_in/login_form.php
+++ log_in/login_form.php
@@ -6,68 +6,51 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>LGTL Cineplex - Log In</title>
+        <meta name="viewport" content "width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="login_form.css">
+        <!-- Bootstrap CSS library -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">        
+        <style>
+            body{
+                background-color: rgb(16,27,44);
+                width: 100%;
+                height: 100vh;
+            }
+            
+            .form-container{
+                padding:50px 60px;
+                margin-top: 20vh;
+                -webkit-box-shadow: -1px 4px 26px 11px rgba(0,0,0,0.75);
+                -moz-box-shadow: -1px 4px 26px 11px rgba(0,0,0,0.75);
+                box-shadow: -1px 4px 26px 11px rgba(0,0,0,0.75);
+            }
+        </style>
     </head>
     <body>
-        <?php
-        include '../nav_bar/navigation_bar.php';
-        ?>
-        <link rel="stylesheet" href="login_form.css"/>
-        <section class="h-100 gradient-form" style="background-color: #eee;">
-          <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-              <div class="col-xl-10">
-                <div class="card rounded-3 text-black">
-                  <div class="row g-0">
-                    <div class="col-lg-6">
-                      <div class="card-body p-md-5 mx-md-4">
-
-                        <div class="text-center">
-                          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp" style="width: 185px;" alt="logo">
-                          <h4 class="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-12"></div>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <form class="form-container bg-dark text-white">
+                        <h1>Welcome back!</h1>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Input your email">
                         </div>
-
-                        <form>
-                          <p>Please login to your account</p>
-
-                          <div class="form-outline mb-4">
-                            <input type="email" id="form2Example11" class="form-control" placeholder="Phone number or email address"/>
-                            <label class="form-label" for="form2Example11">Username</label>
-                          </div>
-
-                          <div class="form-outline mb-4">
-                            <input type="password" id="form2Example22" class="form-control" />
-                            <label class="form-label" for="form2Example22">Password</label>
-                          </div>
-
-                          <div class="text-center pt-1 mb-5 pb-1">
-                            <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log in</button>
-                            <a class="text-muted" href="#!">Forgot password?</a>
-                          </div>
-
-                          <div class="d-flex align-items-center justify-content-center pb-4">
-                            <p class="mb-0 me-2">Don't have an account?</p>
-                            <button type="button" class="btn btn-outline-danger">Create new</button>
-                          </div>
-
-                        </form>
-
-                      </div>
-                    </div>
-                    <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                      <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                        <h4 class="mb-4">We are more than just a company</h4>
-                        <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                      </div>
-                    </div>
-                  </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Input your password">
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Log In</button>
+                    </form>
                 </div>
-              </div>
+                <div class="col-md-4 col-sm-4 col-xs-12"></div>
             </div>
-          </div>
-        </section>
-        <?php
-        include '../nav_bar/footer.php';
-        ?>
+        </div>
     </body>
 </html>
