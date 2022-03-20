f335e90cd48b6c6538576b946c85ef9e0bed7fed
ling63 <liewsl-wm19@student.tarc.edu.my>
2022-03-21 01:26:19 +0800

    l

--- home_page/comingSoon_page.php
+++ home_page/comingSoon_page.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -79,7 +73,7 @@ and open the template in the editor.
             
         </style>
     </head>
-    <body>
+     <body>
         <?php
         include '../nav_bar/navigation_bar.php';
         include 'connection.php';
@@ -136,19 +130,16 @@ and open the template in the editor.
             <div class="position-absolute w-100 top-50 start-0 border-muted border-top"></div>
         </div>
         
-        <div class="row">
+       <div class="row">
             <?php
             $result = mysqli_connect($host, $uname) or die("Could not connect to database." . mysqli_error());
             mysqli_select_db($result, $db_name) or die("Could not select the databse." . mysqli_error());
-            $image_query = mysqli_query($result, "select * from movie WHERE available_status = 'Now Showing' ");
+            $image_query = mysqli_query($result, "select * from movie WHERE available_status = 'Coming Soon' ");
             if (mysqli_num_rows($image_query) > 0) {
                 while ($rows = mysqli_fetch_array($image_query)) {
                     $img_name = $rows['movie_name'];
                     $img_src = $rows['movie_image'];
-//                    $video = $rows['video_link'];
-//                    str_replace("watch?v=", "embed/",$video);
-                    $videourl = $rows['video_link'];
-                    
+                    $video = $rows['video_link'];
                     ?>
             
                     <div class="column col-lg-3 col-md-4 col-xs-6 p-lg-6">
@@ -157,18 +148,17 @@ and open the template in the editor.
                             <img src="data:image/jpg;charset=utf8;base64, <?php echo base64_encode($img_src); ?>" alt="" title="<?php echo $img_name; ?>" class="img card-img-top img-fluid mt-md-2 mt-lg-2 mt-sm-4 rounded float-lg-start " <br>                             
                             <h5 class="text-info mt-lg-2 mt-sm-3 mt-md-2 text-responsive"><?php echo $img_name; ?></h5>  
                             <!--<input type="hidden" name="movie_name" value="<?php echo $img_name; ?>" />-->    
-                            <button id="youtube" onclick="play()" type="submit" class="btn btn-outline-primary btn-circle btn-xl fa fa-play" style=" margin-top:5px;"></button>
+                            <button onclick="play()" value="Input Button" type="button" class="btn btn-outline-primary btn-circle btn-xl fa fa-play" style=" margin-top:5px;"></button>
                             <input id="mdetail" onclick="mdetails()" type="submit" style="margin-top:5px;" class="btn btn-outline-primary" value="Movie Detail"/>  
                             <input type="submit" name="Book_now" style="margin-top:5px; " class="btn btn-outline-success" value="  Book Now  "/>
-                            <!--<video id="myvideo" data-id="<?php echo $videourl ?>"  style="width:100%; height:400px;" type="video/mp4" controls   />-->
-                                    <!--<source  style="width:100%; height:400px;" src="video/<?php // echo $video;   ?>"  />-->
-                                   
-                            <!--</video>-->
+<!--                            <video id="myvideo" data-id="<?php echo $video ?>"  style="width:100%; height:400px;" type="video/mp4" controls   />
+                                    <source  style="width:100%; height:400px;" src="video/<?php // echo $video; ?>"  />
+                                     <iframe class="embed-responsive-item" src="style="width:100%; height:400px;" src="video/<?php echo $video; ?>" allowfullscreen></iframe>
+                            </video>-->
                         </div>  
-                         
-                         <!--</form>-->  
+                        <!--</form>-->  
                     </div>
-                    
+
 
                     <?php
                 }
@@ -188,11 +178,12 @@ and open the template in the editor.
 //        ?>
         
        <script>
-       
-        document.getElementById("youtube").addEventListener("click", page);
-        function videoplay(){
-            echo[ <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $videourl; ?>?autoplay=1&autohide=1&controls=1&showinfo=0&modestbranding=1&rel=0"></iframe>]
+        function play(){
+            var videolink = $('#myvideo').data('video');
+            vidd.get(0).play();
         }
+        
+//         $('#csoon').click(function) {
             document.getElementById("csoon").addEventListener("click", page);
             function page() {
             window.location.href = "http://localhost:8088/LGTL/LGTL_cinema/home_page/comingSoon_page.php";
