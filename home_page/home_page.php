71d89c6854ad7bef8081ca5dc19c363007422780
PLEUTOSEEK <xuanvanquish@gmil.com>
2022-04-04 19:38:57 +0800

    as

--- home_page/home_page.php
+++ home_page/home_page.php
@@ -9,9 +9,8 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Hello, world!</title>
         <link rel="stylesheet" href="home_page.css"/>
+
         <style>
-
-
 
             .srchBar{
                 background-color : #F7F7F7 !important;
@@ -63,10 +62,31 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include '../nav_bar/navigation_bar.php';
         include 'connection.php';
+        include '../nav_bar/navigation_bar.php';
         ?>
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script>
+            $(function () { // on load
+
+                $.ajax({
+                    url: "homePageBackEnd.php", //your page
+                    type: "POST", //type of method
+                    data: {
+                        "action": "retrieveAllMoviesFunc"
+                    }, // passing the values
+                    error: function (xhr, status, error) {
+                        console.log("Error: " + error);
+                    },
+                    success: function (result, status, xhr) {
+                        $("#movie_carts").html(result);
+                    }
+                });
+            });
+        </script>
         <!-- Carousel -->
         <div class="container-fluid mt-3 mt-md-3 mt-sm-6 justify-content-center">
             <!--<div class="row">-->
@@ -99,12 +119,9 @@ and open the template in the editor.
 
                 <!-- The slideshow/carousel -->
                 <div class="carousel-inner">
+
+
                     <?php
-//               $result2 = mysqli_connect($host, $uname) or die("Could not connect to database." . mysqli_error());
-//               mysqli_select_db($result2, $db_name) or die("Could not select the databse." . mysqli_error());
-//               $image_query2 = mysqli_query($result2, "select * from movie WHERE available_status = 'Now Showing' ORDER BY RAND()");
-//                while ($rows = mysqli_fetch_array($image_query2)) {
-//                    $img_src2 = $rows['movie_image'];
                     $i = 0;
                     foreach ($image_query1 as $row) {
                         $actives = '';
@@ -158,15 +175,15 @@ and open the template in the editor.
 
 
         <div class="col-lg-12 input-group mt-3 justify-content-center">
-            <input type="search" class="form-control rounded mr-3 col-lg-4 srchBar" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-            <button type="button" class="btn btn-outline-primary srchBtn ">search</button>
+            <input type="search" class="form-control rounded mr-3 col-lg-4 srchBar" placeholder="Search" aria-label="Search" aria-describedby="search-addon" id = "search-text"/>
+            <button type="button" class="btn btn-outline-primary srchBtn  " id="serch-btn">search</button>
             <!--<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">-->
         </div>
 
         <div class="row col-7">
             <h2 class="position-relative mx-2 mt-3 mt-md-3 mt-sm-6 col text-light">Now Showing</h2>
-            <h2 class="position-relative mx-2 mt-3 mt-md-3 mt-sm-6 col text-light" onclick="window.location.href='comingSoon_page.php'">Coming Soon</h2>
-         
+            <h2 class="position-relative mx-2 mt-3 mt-md-3 mt-sm-6 col text-light" onclick="window.location.href = 'comingSoon_page.php'">Coming Soon</h2>
+
         </div>
 
         <div class="py-2 my-1 text-center position-relative mx-2">
@@ -176,68 +193,10 @@ and open the template in the editor.
             <div class="position-absolute w-100 top-50 start-0 border-muted border-top"></div>
         </div>
 
-        <div class="row">
-            <?php
-            $image_query = mysqli_query($result, "select * from movie WHERE available_status = 'Now Showing'");
-            if (mysqli_num_rows($image_query) > 0) {
-                while ($rows = mysqli_fetch_array($image_query)) {
-                    $img_id = $rows['movie_id'];
-                    $img_name = $rows['movie_name'];
-                    $img_src = $rows['movie_image'];
-                    $videourl = $rows['video_link'];
-                    ?>
-
-                    <div class=" col-lg-3 col-md-4 col-xs-6 p-lg-6">
-                        <!--$uniFormID = uniqid();-->
-                        <!--<form method="post" action="index.php?action=add&id=<?php echo $row["imageID"]; ?>">-->
-                        <div style="border:1px solid #333; background-color:white; border-radius:6px; padding:14px; border-width:1px;" align="center" class="rounded mt-lg-3 mt-4 mt-md-2 my-sm-3 col-lg-11 ">
-                            <img src="data:image/jpg;charset=utf8;base64, <?php echo base64_encode($img_src); ?>" alt="" title="<?php echo $img_name; ?>" class="img card-img-top img-fluid mt-md-2 mt-lg-2 mt-sm-4 rounded float-lg-start " <br>
-                            <h5 class="text-info mt-lg-2 mt-sm-3 mt-md-2 text-responsive"><?php echo $img_name; ?></h5>
-                            <!--<input type="hidden" name="movie_name" value="<?php echo $img_name; ?>" />-->
-                            <button type="button" id="trailer" class="btn btn-outline-primary btn-circle btn-xl fa fa-play" style=" margin-top:5px;" data-toggle="modal" data-target="#myModal2<?php echo $videourl ?>"></button>
-                            <div class="modal fade" id="myModal2<?php echo $videourl ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-lg modal-xl-1140" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title text-info" id="exampleModalLabel"><?php echo $img_name; ?></h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-
-                                        </div>
-                                        <iframe class="ml-lg-2 mt-lg-8 mt-4 mt-md-2 my-sm-3 mr-2" height="400px" src="https://www.youtube.com/embed/<?php echo $videourl ?>?autoplay=1&autohide=1&controls=1&showinfo=0&modestbranding=1&rel=0"></iframe>
-                                    </div>
-                                </div>
-                            </div>
-                            <form action ="../movie_details/movie_page.php" method="post">
-                                <input  type="hidden"  name = "movie_id"  value="<?php echo $img_id; ?>"/>
-                                <input  type="submit" style="margin-top:5px;" class="btn btn-outline-primary" value="Movie Detail"/>
-                            </form>
-                            <form action ="../ticket_booking/selectLocation.php" method="post">
-                                <input  type="hidden"  name = "movie_id"  value="<?php echo $img_id; ?>"/>
-                                <input type="submit"  style="margin-top:5px; " class="btn btn-outline-success" value="  Book Now  "/>
-                            </form>
-
-                        </div>
-                    </div>
-
-                    <?php
-                }
-            }
-            ?>
+        <div class="row" id = "movie_carts">
 
         </div>
-        //<?php
-//       $result = mysqli_connect($host, $uname) or die("Could not connect to database." . mysqli_error());
-//            mysqli_select_db($result, $db_name) or die("Could not select the databse." . mysqli_error());
-//            $image_query = mysqli_query($result, "select videolink from homeimage");
-//        while ($rows = mysqli_fetch_array($image_query)) {
-//            $vid =$rows["videolink"];
-//        }
-//
-        ?>
+
         <script>
 
 
@@ -247,5 +206,23 @@ and open the template in the editor.
         include '../nav_bar/footer.php';
         ?>
 
+        <script>
+            $("#serch-btn").on("click", function () {
+                $.ajax({
+                    url: "homePageBackEnd.php", //your page
+                    type: "POST", //type of method
+                    data: {
+                        "action": "retrieveAllMoviesBasedOnGenreFunc",
+                        "movie_genre_search": $("#search-text").val()
+                    }, // passing the values
+                    error: function (xhr, status, error) {
+                        console.log("Error: " + error);
+                    },
+                    success: function (result, status, xhr) {
+                        $("#movie_carts").html(result);
+                    }
+                });
+            })
+        </script>
     </body>
 </html>
