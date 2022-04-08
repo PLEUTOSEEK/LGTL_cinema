88686555af28da361a33fb900afab1486b6160e9
limxy-wm19 <limxy-wm19@student.tarc.edu.my>
2022-04-08 09:31:44 +0800

    as

--- register/profile.php
+++ register/profile.php
@@ -10,10 +10,8 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"/>
-
+        
         <!-- font awesome -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
 
@@ -23,11 +21,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <title>LGTL Cineplex - Profile</title>
     </head>
     <body>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
         
         <?php
         include '../nav_bar/navigation_bar.php';
@@ -36,7 +29,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <div class="container rounded mt-5 mb-5 text-white">
             <div class="row">
                 <div class="col-md-5 border-right">
-                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="avatar rounded-circle mt-5" src="./img/avatar0.png"><span class="font-weight-bold text-white"><input type="file" id="imgupload" style="display:none"/><button class="btn btn-primary mt-4" id="OpenImgUpload">Change Avatar</button></span><span> </span></div>
+                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="avatar rounded-circle mt-5" src="./img/avatar0.png"><span class="font-weight-bold text-white"><input type="file" id="imgupload" style="display:none"/><button class="btn btn-primary mt-4" id="OpenImgUpload" disabled="">Change Avatar</button></span><span> </span></div>
                 </div>
                 <div class="col-md-6 border-left ps-5">
                     <div class="p-3 pt-5 pb-3">
@@ -44,23 +37,23 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                             <h4 class="text-right">Profile Settings</h4>
                         </div>
                         <div class="row mt-3">
-                            <div class="col-md-12"><label class="labels">Username</label><input type="text" class="form-control" value=""></div>
-                            <div class="col-md-12"><label class="labels">PhoneNumber</label><input type="tel" class="form-control" value=""></div>
-                            <div class="col-md-12"><label class="labels">Email</label><input type="email" class="form-control" value=""></div>
+                            <div class="col-md-12"><label class="labels">Username</label><input type="text" class="form-control" value="" disabled="true"></div>
+                            <div class="col-md-12"><label class="labels">PhoneNumber</label><input type="tel" class="form-control" value="" disabled="true"></div>
+                            <div class="col-md-12"><label class="labels">Email</label><input type="email" class="form-control" value="" disabled="true"></div>
                             <div class="col-md-12"><label class="labels">Password</label>
                                 <div class="row">
-                                    <input type="password" id="password" class="form-control col-9 ms-3" value="">
-                                    <button class="btn bg-white text-muted col-2 ms-2" onclick="password_show_hide();">
+                                    <input type="password" id="password" class="pw form-control col-9 ms-3" value="" disabled="true">
+                                    <button class="btn bg-white text-muted col-2 ms-2 show-hide-btn" onclick="password_show_hide();" disabled="true">
                                         <i class="fas fa-eye" id="show_eye"></i>
                                         <i class="fas fa-eye-slash d-none" id="hide_eye"></i> 
                                     </button>
                                 </div>
                             </div>
                         </div>
-                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Edit Profile</button></div>
                     </div>
-                    <hr>
-                    <div class="my-4 text-center"><button class="btn btn-primary profile-button" type="button">View My Purchase</button></div>
+                    <hr style="background: white">
+                    <div class="my-4 text-center"><button class="btn btn-primary profile-button" type="button">View My Order</button></div>
                 </div>
             </div>
         </div>
