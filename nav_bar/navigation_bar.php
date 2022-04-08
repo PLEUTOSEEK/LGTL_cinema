ee1abfc65b39e8bc2fec973ed2cb92161242bab8
PLEUTOSEEK <xuanvanquish@gmil.com>
2022-04-08 09:11:42 +0800

    as

--- nav_bar/navigation_bar.php
+++ nav_bar/navigation_bar.php
@@ -1,144 +1,134 @@
-<!DOCTYPE html>
-<html>
 
-    <head>
-        <?php
-        session_start();
-        ?>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<?php
+session_start();
+?>
+<!-- Required meta tags -->
 
-        <title>Hello, world!</title>
-        <style>
-            body {
-                margin: 10px;
-                background-color: rgb(16,27,44);
-            }
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-            .nav-link:selected{
-                background-color: #6DD4F9;
-            }
+<style>
+    body {
+        margin: 10px;
+        background-color: rgb(16,27,44);
+    }
 
-            .nav-link:hover{
-                background-color: #314CAA;
-            }
+    .nav-link:selected{
+        background-color: #6DD4F9;
+    }
 
-            .btn-outline-primary:hover{
-                background-color: #314CAA !important;
-            }
+    .nav-link:hover{
+        background-color: #314CAA;
+    }
 
-            .btn-outline-primary:active {
-                background: rgb(0,123,255) !important;
-            }
+    .btn-outline-primary:hover{
+        background-color: #314CAA !important;
+    }
 
-            @media screen and (max-width: 600px) and (max-width:978px){
-                .pl-3{
-                    padding-left:auto;
-                }
-            }
+    .btn-outline-primary:active {
+        background: rgb(0,123,255) !important;
+    }
 
-            /* width */
-            ::-webkit-scrollbar {
-                width: 2px;
-                margin-left: 30px;
-                float: left;
-                height: 4px;
-                background: #fff;
-                overflow-y: scroll;
-                margin-bottom: 25px;
-                border-radius:5px;
-                background-color: yellow;
-            }
+    @media screen and (max-width: 600px) and (max-width:978px){
+        .pl-3{
+            padding-left:auto;
+        }
+    }
 
-            /* Track */
-            ::-webkit-scrollbar-track { /*whole scroll bar*/
+    /* width */
+    ::-webkit-scrollbar {
+        width: 2px;
+        margin-left: 30px;
+        float: left;
+        height: 4px;
+        background: #fff;
+        overflow-y: scroll;
+        margin-bottom: 25px;
+        border-radius:5px;
+        background-color: yellow;
+    }
 
-                background-color: rgb(16,27,44);
-            }
+    /* Track */
+    ::-webkit-scrollbar-track { /*whole scroll bar*/
 
-            /* Handle */
-            ::-webkit-scrollbar-thumb {
-                background: rgb(0,123,255);
-            }
+        background-color: rgb(16,27,44);
+    }
 
-            /* Handle on hover */
-            ::-webkit-scrollbar-thumb:hover {
-                background: white;
-            }
-            @media screen and (max-width: 992px){
-                .scrolling-wrapper {    overflow: hidden;  white-space: nowrap;}
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: rgb(0,123,255);
+    }
 
-            }
-        </style>
-    </head>
-    <body>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: white;
+    }
+    @media screen and (max-width: 992px){
+        .scrolling-wrapper {    overflow: hidden;  white-space: nowrap;}
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark align-items-center pr-lg-0">
-            <a class="navbar-brand   " href="#">LGTL Cineplex</a>
-            <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class=" collapse navbar-collapse col-lg justify-content-center pr-lg-0" id="navbarSupportedContent">
-                <ul class="nav nav-pills  navbar-nav col-lg-9 " id="pills-tab" role="tablist">
-                    <li class="nav-item mr-lg col-lg ">
-                        <a class="nav-link active  mt-lg-1 mt-sm-3 mt-3 text-lg-center  pl-2 pl-lg-0" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
-                    </li>
-                    <li class="nav-item mr-lg col-lg ">
-                        <a class="nav-link   mt-lg-1 mt-sm-3 mt-3 text-lg-center pl-2 pl-lg-0" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
-                    </li>
-                    <li class="nav-item mr-lg col-lg ">
-                        <a class="nav-link    mt-lg-1 mt-sm-3 mt-3 text-lg-center pl-2 pl-lg-0" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
-                    </li>
+    }
+</style>
 
-                </ul>
-                <div class="col-lg-3 col-sm-6 align-self-lg-end float-lg-right" >
-                    <button type="button" id="log-in-btn" class=" btn btn-light float-lg-right  mt-lg-0 mt-sm-3 mt-3 col-sm-4 col-4 d-sm-block ">
-                        <?php
-                        if (isset($_SESSION['logInCustomer'])) {
-                            echo "LOG OUT";
-                        } else {
-                            echo "LOG IN";
-                        }
-                        ?>
-                    </button>
-                </div>
-            </div>
-        </nav>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark align-items-center pr-lg-0">
+    <a class="navbar-brand   " href="#">LGTL Cineplex</a>
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class=" collapse navbar-collapse col-lg justify-content-center pr-lg-0" id="navbarSupportedContent">
+        <ul class="nav nav-pills  navbar-nav col-lg-9 " id="pills-tab" role="tablist">
+            <li class="nav-item mr-lg col-lg ">
+                <a class="nav-link active  mt-lg-1 mt-sm-3 mt-3 text-lg-center  pl-2 pl-lg-0" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+            </li>
+            <li class="nav-item mr-lg col-lg ">
+                <a class="nav-link   mt-lg-1 mt-sm-3 mt-3 text-lg-center pl-2 pl-lg-0" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
+            </li>
+            <li class="nav-item mr-lg col-lg ">
+                <a class="nav-link    mt-lg-1 mt-sm-3 mt-3 text-lg-center pl-2 pl-lg-0" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+            </li>
 
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-
-        <script>
-            $("#log-in-btn").on('click', function (e) {
-                if ($("#log-in-btn").text().trim() == "LOG OUT") {
-                    $.ajax({
-                        type: "POST",
-                        url: "http://localhost/LGTL_Cineplex/LGTL_cinema/nav_bar/navBar_backend.php",
-                        data: {
-                            "action": "unassignCustSESSIONFunc"
-                        },
-                        error: function (xhr, status, error) {
-                            console.log("Error: " + error);
-                        },
-                        success: function (result, status, xhr) {
-                            alert("you have been logged out");
-                            location.reload();
-                        }
-                    });
+        </ul>
+        <div class="col-lg-3 col-sm-6 align-self-lg-end float-lg-right" >
+            <button type="button" id="log-in-btn" class=" btn btn-light float-lg-right  mt-lg-0 mt-sm-3 mt-3 col-sm-4 col-4 d-sm-block ">
+                <?php
+                if (isset($_SESSION['logInCustomer'])) {
+                    echo "LOG OUT";
                 } else {
-                    window.location.href = "http://localhost/LGTL_Cineplex/LGTL_cinema/log_in/login_form.php";
+                    echo "LOG IN";
                 }
-            })
-        </script>
+                ?>
+            </button>
+        </div>
+    </div>
+</nav>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+<script>
+    $("#log-in-btn").on('click', function (e) {
+        if ($("#log-in-btn").text().trim() == "LOG OUT") {
+            $.ajax({
+                type: "POST",
+                url: "http://localhost/LGTL_Cineplex/LGTL_cinema/nav_bar/navBar_backend.php",
+                data: {
+                    "action": "unassignCustSESSIONFunc"
+                },
+                error: function (xhr, status, error) {
+                    console.log("Error: " + error);
+                },
+                success: function (result, status, xhr) {
+                    alert("you have been logged out");
+                    location.reload();
+                }
+            });
+        } else {
+            window.location.href = "http://localhost/LGTL_Cineplex/LGTL_cinema/log_in/login_form.php";
+        }
+    })
+</script>
 
 
-
-    </body>
-
-</html>
